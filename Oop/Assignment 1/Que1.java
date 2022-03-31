c1316845201d2d16b6cda9b2c5eb7d331110d012
sanketrd24 <101094869+sanketrd24@users.noreply.github.com>
2022-03-31 15:21:40 +0530

    Add files via upload

---
+++ Oop/Assignment 1/Que1.java
@@ -0,0 +1,15 @@
+//1. Assign decimal, octal, hexadecimal values to variables and print.
+class Que1{
+	
+	public static void main(String args[]){
+		
+		int a = 100;
+		int b = 0101;
+		char c= '\u0041';
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println((int)c);
+	}
+}
---
+++ Oop/Assignment 1/Que2.java
@@ -0,0 +1,9 @@
+//2. Assign unicode value to char variable and print.
+
+class Que2{
+	public static void main(String args[]){
+		char a='\u0054';
+		System.out.println(a);
+		System.out.println((int)a);
+	}
+}
