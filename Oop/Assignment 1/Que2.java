40e6fbeb654012e743ac8bd1864d5312f4c99246
sanketrd24 <101094869+sanketrd24@users.noreply.github.com>
2022-03-31 15:25:05 +0530

    Delete Oop/Assignment 1 directory

--- Oop/Assignment 1/Que2.java
+++
@@ -1,9 +0,0 @@
-//2. Assign unicode value to char variable and print.
-
-class Que2{
-	public static void main(String args[]){
-		char a='\u0054';
-		System.out.println(a);
-		System.out.println((int)a);
-	}
-}
